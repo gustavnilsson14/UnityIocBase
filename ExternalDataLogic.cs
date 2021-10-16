d66d01622abbf9ed13bf894660980e3f6a517c7d
Gustav Nilsson <gustavnilsson14@gmail.com>
2021-10-16 16:09:03 +0200

    added exlogic and myfileutilhandlerutil

---
+++ ExternalDataLogic.cs
@@ -0,0 +1,105 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Reflection;
+
+public class ExternalDataLogic : InterfaceLogicBase
+{
+    public static ExternalDataLogic I;
+    public static JObject data;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        LoadData();
+    }
+
+    private void LoadData()
+    {
+        JsonSerializer serializer = new JsonSerializer();
+
+        JsonConvert.DefaultSettings = () => new JsonSerializerSettings
+        {
+            ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+        };
+        MyFileHandler.ReadFile(GameFile.CONTENT, out string data, true);
+        ExternalDataLogic.data = JObject.Parse(data);
+    }
+
+    public static JToken GetExternalData(string path)
+    {
+        return ExternalDataLogic.data.SelectToken(path);
+    }
+
+    public static JToken GetExternalDataList(string type) {
+        return ExternalDataLogic.data.SelectToken($"{type}");
+    }
+
+    public static void ApplyDataToGameobject(BehaviourBase target, JToken jToken) {
+        target.name = (string)jToken["name"];
+        Dictionary<string, object> variables = jToken["variables"].ToObject<Dictionary<string, object>>();
+        foreach (string key in variables.Keys)
+        {
+            object value = variables[key];
+            if (value is JObject)
+                value = GetComplexValue(value as JObject);
+            ApplyPrimitiveValue(target, key, value);
+        }
+        if (jToken["children"] == null)
+            return;
+        List<JToken> children = jToken["children"].ToObject<List<JToken>>();
+        foreach (JToken childToken in children)
+        {
+            ApplyDataToChildType(target, childToken);
+        }
+    }
+
+    private static void ApplyDataToChildType(BehaviourBase target, JToken childToken)
+    {
+        Dictionary<string, object> childTypes = (childToken["types"] as JToken).ToObject<Dictionary<string, object>>();
+        List<BehaviourBase> children = target.GetComponentsInChildren(Type.GetType((string)childToken["component"])).Cast<BehaviourBase>().ToList();
+        foreach (string key in childTypes.Keys)
+        {
+            ApplyDataToGameobject(children.Find(x => x.gameObject.name == key), (JToken)childTypes[key]);
+        }
+    }
+
+    private static object GetComplexValue(JObject o)
+    {
+        object result = "";
+        switch ((string)o["type"])
+        {
+            case "range":
+                float stepSize = 0.1f;
+                if (o["stepSize"] != null)
+                    stepSize = (float)o["stepSize"];
+                result = RandomUtil.RandomFloat((float)o["min"], (float)o["max"]);
+                int numSteps = Mathf.FloorToInt((float)result / stepSize);
+                return (float)numSteps * stepSize;
+            case "intrange":
+                int intStepSize = 1;
+                if (o["stepSize"] != null)
+                    intStepSize = (int)o["stepSize"];
+                result = RandomUtil.RandomInt((int)o["min"], (int)o["max"]);
+                numSteps = Mathf.FloorToInt((int)result / intStepSize);
+                return (int)numSteps * intStepSize;
+        }
+        return result;
+    }
+
+    private static void ApplyPrimitiveValue(BehaviourBase target, string key, object value)
+    {
+        if (!ReflectionUtil.GetFieldInfo(out FieldInfo fieldInfo, target, key)) {
+            return;
+        }
+        fieldInfo.SetValue(target, value);
+    }
+}
+
+public interface IExternalDataInstance {
+    string path { get; set; }
+}
---
+++ Util/MyFileHandler.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class MyFileHandler
+{
+    private static string fileExtension = ".stars";
+
+    public static bool ReadFile(GameFile gameFile, out string data, bool autoCreateIfMissing = false, string defaultData = "")
+    {
+        Debug.Log("LOADING: " + GetGameFilePath(gameFile));
+        data = "";
+        if (!File.Exists(MyFileHandler.GetGameFilePath(gameFile)))
+            return MyFileHandler.FileMissing(gameFile, autoCreateIfMissing, defaultData);
+        StreamReader reader = new StreamReader(MyFileHandler.GetGameFilePath(gameFile));
+        data = reader.ReadToEnd();
+        reader.Close();
+        return true;
+    }
+
+    public static bool FileExists(GameFile gameFile) {
+        if (!File.Exists(MyFileHandler.GetGameFilePath(gameFile)))
+            return false;
+        return true;
+    }
+
+    private static bool FileMissing(GameFile gameFile, bool autoCreateIfMissing, string defaultData) {
+        if (autoCreateIfMissing)
+            MyFileHandler.WriteFile(gameFile, defaultData);
+        return false;
+    }
+
+    public static void WriteFile(GameFile gameFile, string serializedData)
+    {
+        StreamWriter writer = new StreamWriter(MyFileHandler.GetGameFilePath(gameFile), false);
+        writer.Write(serializedData);
+        writer.Flush();
+        writer.Close();
+    }
+
+    public static string GetGameFilePath(GameFile gameFile)
+    {
+        return Application.persistentDataPath + MyFileHandler.GameFileToString(gameFile);
+    }
+
+    public static string GameFileToString(GameFile gameFile) {
+        return "/" + gameFile.ToString() + MyFileHandler.fileExtension;
+    }
+}
+
+public enum GameFile
+{
+    SAVE, CONFIG,
+    CONTENT
+}
+
+[System.Serializable]
+public class SaveListContainer
+{
+    public object dataList;
+
+    public SaveListContainer(object dataList)
+    {
+        //Cause json is object oriented
+        this.dataList = dataList;
+    }
+}
